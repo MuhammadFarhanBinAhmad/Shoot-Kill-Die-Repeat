b4e3fac44fc435aac8c637bd1d23d0be7c421e94
CheekyKnob <44768125+MuhammadFarhanBinAhmad@users.noreply.github.com>
2021-09-12 20:00:00 +0800

    V0.20(Updated)
    Miss out a few prefabs

--- Assets/Assets/Scripts/Room_2.0/ExitPortal.cs
+++ Assets/Assets/Scripts/Room_2.0/ExitPortal.cs
@@ -13,11 +13,14 @@ public class ExitPortal : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<PlayerManager>() !=null || other.name == "TestPlayer")
+        if (other.GetComponent<PlayerManager>() !=null)
         {
+            other.GetComponent<CharacterController>().enabled = false;
             other.gameObject.transform.position = RSV2.portal_Entrance[RSV2.room_Cleared].transform.Find("SpawnPoint").transform.position;
+            other.gameObject.transform.rotation = RSV2.portal_Entrance[RSV2.room_Cleared].transform.Find("SpawnPoint").transform.rotation;
+            other.GetComponent<CharacterController>().enabled = true;
             RSV2.room_Cleared++;
-
+            print("hit");
         }
     }
 }
