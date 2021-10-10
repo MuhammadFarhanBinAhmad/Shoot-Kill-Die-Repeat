b8796ff08624b53fb8667257f5608f89e034925f
CheekyKnob <44768125+MuhammadFarhanBinAhmad@users.noreply.github.com>
2021-10-10 18:40:06 +0800

    V0.02(Enemy&Obstacle)

--- Assets/Assets/Scripts/Room_2.0/MovingPlatform.cs
+++ Assets/Assets/Scripts/Room_2.0/MovingPlatform.cs
@@ -6,9 +6,67 @@ public class MovingPlatform : MonoBehaviour
 {
     [Header("Moving Spot")]
     public List<Transform> check_Point = new List<Transform>();
-    public int current_CheckPoint;
+    public int point_Number = 0;
+    Transform current_Point;
 
+    public float tolerence, speed, delay;
 
+    float start_delay;
+
+    public bool automatic;
+
+    private void Start()
+    {
+        if (check_Point.Count > 0)
+        {
+            current_Point = check_Point[0];
+        }
+        tolerence = speed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position != current_Point.position)
+        {
+            MovePlatform();
+        }
+        else
+        {
+            UpdateTarget();
+        }
+    }
+
+    void MovePlatform()
+    {
+        Vector3 heading = current_Point.position - transform.position;
+        transform.position += (heading / heading.magnitude) * speed * Time.deltaTime;
+        if (heading.magnitude < tolerence)
+        {
+            transform.position = current_Point.position;
+            start_delay = Time.time;
+        }
+    }
+
+    void UpdateTarget()
+    {
+        if (automatic)
+        {
+            if (Time.time - start_delay > delay)
+            {
+                NextPlatform();
+            }
+        }
+    }
+    public void NextPlatform()
+    {
+        print("NEXTPLATFORM");
+        point_Number++;
+        if (point_Number >= check_Point.Count)
+        {
+            point_Number = 0;
+        }
+        current_Point = check_Point[point_Number];
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<TestDummy>() != null)
